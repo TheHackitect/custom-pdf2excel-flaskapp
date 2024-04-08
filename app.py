b9b6df84b1b74d4adb0928f686b8e5ed4f6c51e8
thehackitect <marvellouskolawole.mk@gmail.com>
2024-04-08 09:15:52 +0100

    .

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def download_file():
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    socketio.run(app, port=8080, host="0.0.0.0", debug=True)
+    socketio.run(app, port=8000, host="0.0.0.0", debug=True)
